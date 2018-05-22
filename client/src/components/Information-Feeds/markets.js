71bf2fc6c821a7753ee89a78c4106194f131c8b6
Yoan Yomba <user@n3-119-255.dhcp.drexel.edu>
2018-05-22 19:09:53 -0400

    Added Markets Functionality Fix

--- client/src/components/Information-Feeds/markets.js
+++ client/src/components/Information-Feeds/markets.js
@@ -47,11 +47,16 @@ class Markets extends Component {
     var { data } = this.state.feeds;
     var count = 0;
     let feedItems = [];
+    let temp = [];
 
     let elements = [];
     if (data !== null || data !== undefined) {
       for (var item in data) {
-        data[item].map(value => {
+        data[item].map((value, index) => {
+          var info = data[item];
+          if (index % 2 && index > 0) {
+            temp.push([info[index], info[index - 1]]);
+          }
           if (count < 20) {
             feedItems.push(
               <div className="container">
@@ -89,7 +94,65 @@ class Markets extends Component {
         {this.state.feeds.data === null ? (
           <div>Please wait, loading…</div>
         ) : (
-          <div className="row-3"> {feedItems}</div>
+          <div>
+            {temp.map((item, index) => {
+              console.log(item[0]);
+              return (
+                <div className="row">
+                  <div className="col-sm-6">
+                    <div className="container">
+                      <div className="card flex-md-row mb-4 box-shadow h-md-250">
+                        <div className="card-body d-flex flex-column align-items-start">
+                          <strong className="d-inline-block mb-2 text-primary" />
+                          <h3 className="mb-0">
+                            <a className="text-dark">{item[0].title}</a>
+                            <hr />
+                          </h3>
+                          <h4 className="mb-1 text-muted">{item[0].pubdate}</h4>
+                          <p className="card-text mb-auto">{item[0].content}</p>
+                          <br />
+                          <p className="lead mb-0">
+                            <a
+                              className="text-muted font-weight-bold"
+                              href={item[0].link}
+                            >
+                              {" "}
+                              Continue reading...{" "}
+                            </a>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-sm-6">
+                    <div className="container">
+                      <div className="card flex-md-row mb-4 box-shadow h-md-250">
+                        <div className="card-body d-flex flex-column align-items-start">
+                          <strong className="d-inline-block mb-2 text-primary" />
+                          <h3 className="mb-0">
+                            <a className="text-dark">{item[1].title}</a>
+                            <hr />
+                          </h3>
+                          <h4 className="mb-1 text-muted">{item[1].pubdate}</h4>
+                          <p className="card-text mb-auto">{item[1].content}</p>
+                          <br />
+                          <p className="lead mb-0">
+                            <a
+                              className="text-muted font-weight-bold"
+                              href={item[1].link}
+                            >
+                              {" "}
+                              Continue reading...{" "}
+                            </a>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         )}
       </div>
     );
